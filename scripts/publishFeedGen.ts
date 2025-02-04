e63afca35f6f858b13a5b9b94b6d28a76aaf356d
myspleen <from.my.spleen@gmail.com>
2025-02-04 21:35:47 +0900

    up

--- scripts/publishFeedGen.ts
+++ scripts/publishFeedGen.ts
@@ -1,51 +1,75 @@
 import dotenv from 'dotenv'
-import { AtpAgent, BlobRef } from '@atproto/api'
+import inquirer from 'inquirer'
+import { AtpAgent, BlobRef, AppBskyFeedDefs } from '@atproto/api'
 import fs from 'fs/promises'
 import { ids } from '../src/lexicon/lexicons'
 
 const run = async () => {
   dotenv.config()
 
-  // YOUR bluesky handle
-  // Ex: user.bsky.social
-  const handle = 'estampie.work'
-
-  // YOUR bluesky password, or preferably an App Password (found in your client settings)
-  // Ex: abcd-1234-efgh-5678
-  if (!process.env.FEEDGEN_PUBLISH_APP_PASSWORD) {
-    throw new Error('Please provide an app password in the .env file')
-  }
-  const password = process.env.FEEDGEN_PUBLISH_APP_PASSWORD
-
-  // A short name for the record that will show in urls
-  // Lowercase with no spaces.
-  // Ex: whats-hot
-  const recordName = 'megido'
-
-  // A display name for your feed
-  // Ex: What's Hot
-  const displayName = 'メギド絵フィード'
-
-  // (Optional) A description of your feed
-  // Ex: Top trending content from the whole network
-  const description = '「メギド」+「絵」or「イラスト」が含まれていて画像が添付されている投稿を集めたフィードです。自鯖に設置のため、たまにメンテナンスなどでアクセスできないときがあるのでご注意下さい。'
-
-  // (Optional) The path to an image to be used as your feed's avatar
-  // Ex: ~/path/to/avatar.jpeg
-  const avatar: string = '/publish/mgd.png'
-
-  // -------------------------------------
-  // NO NEED TO TOUCH ANYTHING BELOW HERE
-  // -------------------------------------
-
   if (!process.env.FEEDGEN_SERVICE_DID && !process.env.FEEDGEN_HOSTNAME) {
     throw new Error('Please provide a hostname in the .env file')
   }
+
+  const answers = await inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'handle',
+        message: 'Enter your Bluesky handle:',
+        required: true,
+      },
+      {
+        type: 'password',
+        name: 'password',
+        message: 'Enter your Bluesky password (preferably an App Password):',
+      },
+      {
+        type: 'input',
+        name: 'service',
+        message: 'Optionally, enter a custom PDS service to sign in with:',
+        default: 'https://bsky.social',
+        required: false,
+      },
+      {
+        type: 'input',
+        name: 'recordName',
+        message: 'Enter a short name or the record. This will be shown in the feed\'s URL:',
+        required: true,
+      },
+      {
+        type: 'input',
+        name: 'displayName',
+        message: 'Enter a display name for your feed:',
+        required: true,
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Optionally, enter a brief description of your feed:',
+        required: false,
+      },
+      {
+        type: 'input',
+        name: 'avatar',
+        message: 'Optionally, enter a local path to an avatar that will be used for the feed:',
+        required: false,
+      },
+      {
+        type: 'confirm',
+        name: 'videoOnly',
+        message: 'Is this a video-only feed? If so, do you want to set the content mode to video? This will allow for an "immersive" video experience within the app.',
+        default: false,
+      }
+    ])
+
+  const { handle, password, recordName, displayName, description, avatar, service, videoOnly } = answers
+
   const feedGenDid =
     process.env.FEEDGEN_SERVICE_DID ?? `did:web:${process.env.FEEDGEN_HOSTNAME}`
 
   // only update this if in a test environment
-  const agent = new AtpAgent({ service: 'https://bsky.social' })
+  const agent = new AtpAgent({ service: service ? service : 'https://bsky.social' })
   await agent.login({ identifier: handle, password })
 
   let avatarRef: BlobRef | undefined
@@ -75,6 +99,7 @@ const run = async () => {
       description: description,
       avatar: avatarRef,
       createdAt: new Date().toISOString(),
+      contentMode: videoOnly ? AppBskyFeedDefs.CONTENTMODEVIDEO : AppBskyFeedDefs.CONTENTMODEUNSPECIFIED,
     },
   })
 
